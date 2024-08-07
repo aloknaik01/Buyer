d869786e70349332d40640d450d3eb498de2504f
Alok kumar Naik <aloknaik016261@gmail.com>
2024-08-07 17:24:32 +0530

    add dummy sample react vite e-commerce web app

--- src/app/store.js
+++ src/app/store.js
@@ -3,7 +3,7 @@ import categorySlice from "../features/categorySlice";
 import cartSlice from "../features/cartSlice";
 import inputSlice from "../features/inputSlice";
 import pageCountSlice from "../features/pageCountSlice";
-import getObjectSlice from "../features/getObject";
+
 
 
 export const store = configureStore({
@@ -12,7 +12,5 @@ export const store = configureStore({
     cart: cartSlice,
     query: inputSlice,
     page: pageCountSlice,
-    obj: getObjectSlice,
-
   },
 });
